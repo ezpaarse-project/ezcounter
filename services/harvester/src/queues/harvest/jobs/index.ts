f1fe6e53845cbd5d5357e6a2316054fd334fa7fb
oxypomme <oxytom8@gmail.com>
2026-02-27 11:13:10 +0100

    fix(harvester): marking jobs as delayed

--- services/harvester/src/queues/harvest/jobs/index.ts
+++ services/harvester/src/queues/harvest/jobs/index.ts
@@ -12,6 +12,8 @@ import { config } from '~/lib/config';
 
 import { harvestReport } from '~/models/report';
 
+import { sendHarvestJobStatusEvent } from './status';
+
 const logger = appLogger.child({ scope: 'queues' });
 
 const {
@@ -22,33 +24,103 @@ const {
   maxTries,
 } = config.download;
 
+const delayedJobs = new Map<string, Set<string>>();
+
+/**
+ * Mark harvest job as processing
+ *
+ * @param job - The job
+ * @param queueName - The name of the queue
+ */
+function markHarvestJobAsProcessing(
+  job: HarvestJobData,
+  queueName: string
+): void {
+  sendHarvestJobStatusEvent({
+    id: job.id,
+    status: 'processing',
+  });
+
+  // Keep track of delayed jobs
+  const delayed = delayedJobs.get(queueName);
+  if (delayed?.has(job.id)) {
+    delayed.delete(job.id);
+    delayedJobs.set(queueName, delayed);
+  }
+}
+/**
+ * Mark harvest job as delayed
+ *
+ * @param job - The job
+ * @param queueName - The name of the queue
+ */
+function markHarvestJobAsDelayed(job: HarvestJobData, queueName: string): void {
+  sendHarvestJobStatusEvent({
+    id: job.id,
+    status: 'delayed',
+  });
+
+  // Keep track of delayed jobs
+  const delayed = delayedJobs.get(queueName) ?? new Set();
+  delayed.add(job.id);
+  delayedJobs.set(queueName, delayed);
+}
+
+type RequeueData = {
+  /** The harvest job */
+  job: HarvestJobData;
+  /** The queue where to requeue the job */
+  queueName: string;
+  /** The delay to apply to message, none by default */
+  delay?: number;
+  /** The time to pause queue, none by default */
+  pause?: number;
+};
+
 /**
  * Requeue harvest job with some delay
  *
  * @param channel - The rabbitmq channel
- * @param data - Information about message
- * @param delay - The delay to apply
+ * @param data - How to requeue job
  */
-function requeueHarvestJob(
+async function requeueHarvestJob(
   channel: rabbitmq.Channel,
-  data: { msg: HarvestJobData; queueName: string },
-  delay = 0
-): void {
+  data: RequeueData
+): Promise<void> {
+  const pause = data.pause ? Math.max(1, data.pause) : 0;
+  const delay = data.delay ? Math.max(1, data.delay) : undefined;
+
+  // Pause whole queue if needed
+  if (pause > 0) {
+    logger.info({
+      msg: 'Pausing queue',
+      queueName: data.queueName,
+      pause,
+    });
+    // We're having one dispatch for each data host, and don't ack dispatch until all jobs are completed,
+    // we're using `prefetch=1` on both connections to ensure that only one job per data host is processed
+    // at the same time.
+    // Meaning that if any job hangs, it'll block the whole job queue and prevent any other harvester to
+    // pick it up.
+    // As if data host is busy every request to it will fail, we want to block the whole queue here
+    await sleep(pause);
+  }
+
   try {
-    sendJSONMessage({ channel, queue: { name: data.queueName } }, data.msg, {
+    sendJSONMessage({ channel, queue: { name: data.queueName } }, data.job, {
       headers: { 'x-delay': delay },
     });
     logger.info({
       msg: 'Harvest job requeued',
       queueName: data.queueName,
-      id: data.msg.id,
+      id: data.job.id,
       delay,
     });
   } catch (err) {
     logger.error({
       msg: 'Failed to requeue job',
       queueName: data.queueName,
-      id: data.msg.id,
+      id: data.job.id,
       err,
     });
   }
@@ -78,34 +150,28 @@ async function onMessage(
     return;
   }
 
+  // Mark job as processing
+  markHarvestJobAsProcessing(data, queueName);
+
   // Just a little delay to avoid spamming too fast
   await sleep(jobDelay);
 
   data.try = (data.try ?? 0) + 1;
   const result = await harvestReport(data);
 
-  if (data.try < maxTries) {
-    // Data host is currently processing report
-    if (result.processing) {
-      // Requeue job
-      requeueHarvestJob(channel, { msg: data, queueName }, processingBackoff);
-      channel.ack(msg);
-      return;
-    }
-    // Data host is currently unavailable
-    if (result.unavailable) {
-      // We're having one dispatch for each data host, and don't ack dispatch until all jobs are completed,
-      // we're using `prefetch=1` on both connections to ensure that only one job per data host is processed
-      // at the same time.
-      // Meaning that if any job hangs, it'll block the whole job queue and prevent any other harvester to
-      // pick it up.
-      // As if data host is busy every request to it will fail, we want to block the whole queue here
-      await sleep(unavailableBackoff);
-      // Once delay passed, we can requeue job
-      requeueHarvestJob(channel, { msg: data, queueName });
-      channel.ack(msg);
-      return;
-    }
+  // We need to requeue report and we have enough tries left
+  if ((result.processing || result.unavailable) && data.try < maxTries) {
+    // Mark job as delayed - It'll be picked up later
+    markHarvestJobAsDelayed(data, queueName);
+    // Requeue job
+    await requeueHarvestJob(channel, {
+      job: data,
+      queueName,
+      // Data host is currently processing report - we'll retry later
+      delay: result.processing ? processingBackoff : undefined,
+      // Data host is currently unavailable - pausing whole data host
+      pause: result.unavailable ? unavailableBackoff : undefined,
+    });
   }
   // Acknowledge message as it was successfully processed
   channel.ack(msg);
@@ -123,20 +189,32 @@ async function detachHarvestJobsQueue(
   channel: rabbitmq.Channel,
   queueName: string
 ): Promise<boolean> {
+  // Don't detach if there's still delayed jobs
+  const delayed = delayedJobs.get(queueName);
+  if (delayed && delayed.size > 0) {
+    return false;
+  }
+
   try {
-    const { messageCount } = await channel.deleteQueue(queueName, {
+    // Check if there's messages in queue
+    const { messageCount } = await channel.checkQueue(queueName);
+    if (messageCount > 0) {
+      return false;
+    }
+
+    // Delete queue - ifEmpty will close channel if there's still jobs
+    await channel.deleteQueue(queueName, {
       ifEmpty: true,
     });
 
-    if (messageCount <= 0) {
-      logger.info({
-        msg: 'Harvest queue detached',
-        queueName,
-      });
-      return true;
-    }
+    logger.info({
+      msg: 'Harvest queue detached',
+      queueName,
+    });
 
-    return false;
+    // Queue was deleted, no need to track delayed jobs
+    delayedJobs.delete(queueName);
+    return true;
   } catch (err) {
     logger.error({
       msg: 'Unable to detach queue',
