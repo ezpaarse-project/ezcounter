b52c0ffd806243343e077288681f46a7e73317ea
oxypomme <tom.sublet@proton.me>
2026-09-08 09:38:58 +0200

    build(database): using POSTGRES_URL _USERNAME and _PASSWORD for db to match other services

--- packages/database/prisma.config.ts
+++ packages/database/prisma.config.ts
@@ -1,10 +1,14 @@
 import { join } from 'node:path';
 
-import { defineConfig } from 'prisma/config';
+import { defineConfig, env } from 'prisma/config';
+
+const url = new URL(env('POSTGRES_URL'));
+url.username = env('POSTGRES_USERNAME');
+url.password = env('POSTGRES_PASSWORD');
 
 export default defineConfig({
   datasource: {
-    url: process.env.DATABASE_URL,
+    url: url.href,
   },
 
   schema: join('prisma'),
